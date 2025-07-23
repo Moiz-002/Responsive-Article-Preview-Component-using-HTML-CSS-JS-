62d2f561ed53198d5957bc0ee585f20cdef73255
Abdul Moiz <abdulmoizcoms002@gmail.com>
2025-07-23 05:47:03 -0700

    Initial Commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let share_button = document.querySelector(".share-button");
-let share_options = document.querySelector(".share-options");
+const share_button = document.querySelector(".share-button");
+const share_options = document.querySelector(".share-options");
 
 share_button.addEventListener('click', (event) =>{
     share_options.classList.toggle('hidden');
